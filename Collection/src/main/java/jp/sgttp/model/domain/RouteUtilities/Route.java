40c4fb64e4e09e12aeaae19c0b7c7c6fe471844d
JPabloCarvajal <juanpablocarvajalgiraldo19@gmail.com>
2024-03-31 20:01:53 -0500

    Se agrega el mapa de grafos de las rutas, ademas del metodo de recomendar la ruta mas corta entre estaciones

--- Collection/src/main/java/jp/sgttp/model/domain/RouteUtilities/Route.java
+++ Collection/src/main/java/jp/sgttp/model/domain/RouteUtilities/Route.java
@@ -2,19 +2,22 @@ package jp.sgttp.model.domain.RouteUtilities;
 
 import jp.linkedlist.singly.LinkedList;
 import jp.queue.list.QueueList;
+import jp.sgttp.model.domain.trainUtilities.Train;
 
 public class Route {
 
     private int routeId;
     private LinkedList<Station> stations;
     private QueueList<Station> actualRoute;
+    private QueueList<Train> actualTrain;
     private Station destinationPoint;
     private Station startPoint;
     private float kmTraveled;
     private float kmToEachStation;
 
     public Route(int routeId, LinkedList<Station> stations, QueueList<Station> actualRoute,
-                 Station destinationPoint, Station startPoint, float kmTraveled, float kmToEachStation) {
+                 Station destinationPoint, Station startPoint, float kmTraveled, float kmToEachStation,
+                 QueueList<Train> actualTrain) {
         this.routeId = routeId;
         this.stations = stations;
         this.actualRoute = actualRoute;
@@ -22,6 +25,13 @@ public class Route {
         this.startPoint = startPoint;
         this.kmTraveled = kmTraveled;
         this.kmToEachStation = kmToEachStation;
+        this.actualTrain = actualTrain;
+    }
+    public QueueList<Train> getActualTrain() {
+        return actualTrain;
+    }
+    public void setActualTrain(QueueList<Train> actualTrain) {
+        this.actualTrain = actualTrain;
     }
 
     public int getRouteId() {
--- Collection/src/main/java/jp/sgttp/model/domain/RouteUtilities/RoutesMap.java
+++ Collection/src/main/java/jp/sgttp/model/domain/RouteUtilities/RoutesMap.java
@@ -1,5 +1,86 @@
 package jp.sgttp.model.domain.RouteUtilities;
 
+import jp.array.Array;
+import jp.graphs.*;
+import jp.queue.array.QueueArray;
+
 public class RoutesMap {
-    //Grafo xd
+
+    Graph<Station> stationsGraph = new Graph<>();
+
+    Station stationA = new Station("Estacion A", "Ciudad A");
+    Station stationB = new Station("Estacion B", "Ciudad B");
+    Station stationC = new Station("Estacion C", "Ciudad C");
+    Station stationD = new Station("Estacion D", "Ciudad D");
+    Station stationE = new Station("Estacion E", "Ciudad E");
+    Station stationF = new Station("Estacion F", "Ciudad F");
+    Station stationG = new Station("Estacion G", "Ciudad G");
+    Station stationH = new Station("Estacion H", "Ciudad H");
+    Station stationI = new Station("Estacion I", "Ciudad I");
+    Station stationJ = new Station("Estacion J", "Ciudad J");
+    Station stationK = new Station("Estacion K", "Ciudad K");
+
+    public RoutesMap() {
+        stationsGraph.newNode(stationA);
+        stationsGraph.newNode(stationB);
+        stationsGraph.newNode(stationC);
+        stationsGraph.newNode(stationD);
+        stationsGraph.newNode(stationE);
+        stationsGraph.newNode(stationF);
+        stationsGraph.newNode(stationG);
+        stationsGraph.newNode(stationH);
+        stationsGraph.newNode(stationI);
+        stationsGraph.newNode(stationJ);
+        stationsGraph.newNode(stationK);
+
+        // Conexiones 
+        stationsGraph.newEdge(stationA, stationF, 50);
+        stationsGraph.newEdge(stationA, stationD, 50);
+        stationsGraph.newEdge(stationA, stationC, 40);
+        stationsGraph.newEdge(stationA, stationB, 30);
+        
+        stationsGraph.newEdge(stationB, stationA, 30);
+
+        stationsGraph.newEdge(stationC, stationA, 40);
+        stationsGraph.newEdge(stationC, stationI, 80);
+        stationsGraph.newEdge(stationC, stationJ, 120);
+        stationsGraph.newEdge(stationC, stationK, 110);
+
+        stationsGraph.newEdge(stationD, stationA, 50);
+        stationsGraph.newEdge(stationD, stationE, 20);
+
+        stationsGraph.newEdge(stationE, stationD, 20);
+        stationsGraph.newEdge(stationE, stationF, 65);
+
+        stationsGraph.newEdge(stationF, stationA, 50);
+        stationsGraph.newEdge(stationF, stationE, 65);
+        stationsGraph.newEdge(stationF, stationG, 80);
+
+        stationsGraph.newEdge(stationG, stationF, 80);
+        stationsGraph.newEdge(stationG, stationH, 30);
+        stationsGraph.newEdge(stationG, stationI, 145);
+
+        stationsGraph.newEdge(stationH, stationG, 30);
+        
+        stationsGraph.newEdge(stationI, stationG, 145);
+        stationsGraph.newEdge(stationI, stationC, 80);
+
+        stationsGraph.newEdge(stationJ, stationC, 120);
+
+        stationsGraph.newEdge(stationK, stationC, 110);
+    }
+
+    public float lowestDistanceBeetweenStationsKM(Station A, Station B){
+        return stationsGraph.shortestPath(stationsGraph, A, B);
+    }
+
+    public QueueArray<Station> stationsToTravel(Station A,Station B){
+        Array<GraphNode<Station>> shortestPathNodes = stationsGraph.shortestPathNodes(stationsGraph, A, B);
+        QueueArray<Station> stations = new QueueArray<>(shortestPathNodes.size());
+        for (int i = 0; i < shortestPathNodes.size(); i++) {
+            stations.insert(shortestPathNodes.get(i).data);
+        }
+        return stations;
+    }
+    
 }
