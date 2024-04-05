3d6f1819a337918ed880b809ce45866eb2e91db4
JPabloCarvajal <juanpablocarvajalgiraldo19@gmail.com>
2024-04-05 12:51:06 -0500

    organizacion y limpieza de codigo

--- Collection/src/main/java/jp/sgttp/controller/SGTTPController.java
+++ Collection/src/main/java/jp/sgttp/controller/SGTTPController.java
@@ -7,18 +7,7 @@ import jp.sgttp.model.domain.Main;
 import jp.sgttp.view.SGTTPView;
 
 public class SGTTPController {
-
-//    private SGTTPModel model;
-//    private SGTTPView view;
-//  
-//    public SGTTPController(SGTTPModel model, SGTTPView view) {
-//      this.model = model;
-//      this.view = view;
-//    }
-//  
-//    public void start() {
-//      view.init(new String[]{model.getTitle()});
-//    }
+    
     private final Login view;
 
     public SGTTPController(Login view) {
